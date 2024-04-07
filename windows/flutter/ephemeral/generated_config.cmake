6ea7cf602e0c6ab67ef7b4dfaacb0af859864e2b
omparshetti <117383000+omparshetti@users.noreply.github.com>
2024-04-07 14:26:35 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\App\\digilocker" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Downloads\\flutter"
+  "PROJECT_DIR=F:\\App\\digilocker"
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\App\\digilocker\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\App\\digilocker"
+  "FLUTTER_TARGET=F:\\App\\digilocker\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\App\\digilocker\\.dart_tool\\package_config.json"
+)
